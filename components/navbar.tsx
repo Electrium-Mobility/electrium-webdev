a5c8453dcdcd40ad29a87fb10a30f99783a401a9
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-12-06 12:20:38 -0500

    fixed thing vercel didn't like

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,15 @@
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image"
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <>
         <div className={styles.navbar}>
-            <a className={styles.logo} href="/"><Image src="/logo.png" alt="logo" width={72} height={72}></Image></a>
-            <a className={styles.item} href="/about">About</a>
-            <a className={styles.item} href="/team">Team</a>
-            <a className={styles.item} href="/sponsors">Sponsors</a>
+            <Link className={styles.logo} href="/"><Image src="/logo.png" alt="logo" width={72} height={72}></Image></Link>
+            <Link className={styles.item} href="/about">About</Link>
+            <Link className={styles.item} href="/team">Team</Link>
+            <Link className={styles.item} href="/sponsors">Sponsors</Link>
         </div>
         
         </>
