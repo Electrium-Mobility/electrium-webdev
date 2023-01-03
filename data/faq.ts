e8e6cd34e8c247f31f680007a0a01591ea0dd8eb
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-01-03 14:36:55 -0500

    join complete mostly

---
+++ data/faq.ts
@@ -0,0 +1,15 @@
+export const faq = [
+    {
+        question: "question",
+        answer: "answer"
+    },
+    {
+        question: "super super very long question that takes up a lot of space super super very long question that takes up a lot of space super super very long question that takes up a lot of space super super very long question that takes up a lot of space",
+        answer: "super duper very long answer that takes upa lot of space super duper very long answer that takes upa lot of space super duper very long answer that takes upa lot of space super duper very long answer that takes upa lot of space"
+    }
+]
+
+export interface QA {
+    question: string,
+    answer: string
+}
--- pages/join.tsx
+++ pages/join.tsx
@@ -3,6 +3,8 @@ import JoinCard from "../components/join-card"
 import Layout from "../components/layout"
 import styles from "../styles/Join.module.scss"
 import variables from "../styles/_next_access.module.scss"
+import Image from "next/image"
+import { faq, QA } from "../data/faq"
 
 export default function Join() {
     const content = (
@@ -20,6 +22,46 @@ export default function Join() {
             <JoinCard image={"/logo.png"} title={"Impact"} description={"Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}></JoinCard>
             <JoinCard image={"/logo.png"} title={"Learning"} description={"Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}></JoinCard>
             <JoinCard image={"/logo.png"} title={"Networking"} description={"Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}></JoinCard>
+        </div>
+        <div className={styles.howApply}>
+            <h2>How to Apply?</h2>
+            <div className={styles.block}>
+                <div className={styles.greyCircle}>1</div>
+                <h3>Submit your Application</h3>
+                Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+            <div className={styles.block}>
+                <Image src="/logo.png" alt="submit" fill></Image>
+            </div>
+            <div className={styles.block}>
+                <Image src="/logo.png" alt="submit" fill></Image>
+            </div>
+            <div className={styles.block}>
+                <div className={styles.greyCircle}>2</div>
+                <h3>Interview with us</h3>
+                Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+            <div className={styles.block}>
+                <div className={styles.greyCircle}>3</div>
+                <h3>Wait for decision</h3>
+                Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+            <div className={styles.block}>
+                <Image src="/logo.png" alt="submit" fill></Image>
+            </div>
+        </div>
+        <div className={styles.faq}>
+            <h2>FAQ</h2>
+            {faq.map((qa:QA, i:number) => (
+                <details className={styles.question}>
+                    <summary><p className={styles.questionText}>{qa.question}</p><p className={styles.arrowDown}>&#x2228;</p></summary>
+                    <p>{qa.answer}</p>
+                </details>
+            ))}
+        </div>
+        <div className={styles.interested}>
+            <h2>Interested in our work?</h2>
+            <Button backgroundColour={variables.buttonA} href={""} content={"Apply now"} textColour={variables.buttonB}></Button>
         </div>
         </>
     )
