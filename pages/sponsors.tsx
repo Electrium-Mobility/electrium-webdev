3dc3f33c2a9c7a20a89ec807c4797e88e971dde6
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-01-07 14:34:08 -0500

    sponsors page

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,5 +1,38 @@
 import Layout from "../components/layout"
+import styles from "../styles/Sponsors.module.scss"
+import Button from "../components/button"
+import variables from "../styles/_next_access.module.scss"
+import { Sponsor, sponsors } from "../data/sponsors"
+import Image from "next/image"
 
 export default function Sponsors() {
-    return(<Layout{...{content:<>Sponsors</>, name:"Sponsors"}}/>)
+    const content = (
+        <>
+        <div className={styles.topSection}>
+            <div className={styles.topLeftSide}>
+                <h1>Our Sponsors</h1>
+                <p>Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar.</p>
+            </div>
+            <Button backgroundColour={variables.buttonB} href={""} content={"Become a Sponsor"} textColour={variables.buttonA}></Button>
+        </div>
+        <div className={styles.imagesSection}>
+            {sponsors.map((sponsor: Sponsor, i: number) => (
+                <div className={styles.imageContainer}>
+                    <Image key={i} src={sponsor.image} alt={sponsor.name} fill></Image>
+                </div>
+            ))}
+        </div>
+        <div className={styles.bottomSection}>
+            <div className={styles.textContainer}>
+                <h2>Contact us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur. Tempor dis duis pulvinar. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>electriummobility@gmail.com</p>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src="/logo.png" alt="idk" fill></Image>
+            </div>
+        </div>
+        </>
+    )
+    return(<Layout{...{content:content, name:"Sponsors"}}/>)
 }
